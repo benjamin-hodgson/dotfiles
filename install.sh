5c27f195b5cf954c2daaa05aeb1c155bd2ad398c
Benjamin Hodgson <3857587+benjamin-hodgson@users.noreply.github.com>
2022-06-14 16:27:12 +0000

    ??

--- install.sh
+++ install.sh
@@ -6,5 +6,3 @@ ln -s ${BASEDIR}/gitconfig ~/.gitconfig
 ln -s ${BASEDIR}/inputrc ~/.inputrc
 ln -s ${BASEDIR}/zshenv ~/.zshenv
 ln -s ${BASEDIR}/zshrc ~/.zshrc
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
